d40ac62b4d2b64ff489bd0d3fa351f4a0a93c7b5
박은정 <beanlove97@gmail.com>
2021-12-02 13:24:38 +0900

    Test: mock server 새로 추가

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,2 +1,3 @@
 export const url =
-  'https://14a2ad92-4770-4fe1-92dd-4ba6f3d3b3a1.mock.pstmn.io/';
+  // 'https://14a2ad92-4770-4fe1-92dd-4ba6f3d3b3a1.mock.pstmn.io/';
+  'https://c83771e4-f163-4d8f-8657-9a85ed6b1eb0.mock.pstmn.io';
